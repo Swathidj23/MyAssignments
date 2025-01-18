ff2a95c004ef45e0f509ef35b90b21c2e921ae6d
Swathidj23 <swathidj23@gmail.com>
2025-01-18 23:30:01 +0530

    Add files via upload
    Day 1 Home Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0F;
+		String developer ="Google";
+		boolean isBeta = false;
+		int releaseYear =2008;
+		char shortcutKey ='C';
+		System.out.println("version :"+version+"\ndeveloper :"+developer+"\nisBeta :"+isBeta+"\nreleaseYear :"+releaseYear+"\nshortcutKey :"+shortcutKey);
+		
+		
+
+	}
+
+}
