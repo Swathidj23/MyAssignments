0cc8cc14174feb1640bd426f2b6a7bc534f07d57
Swathidj23 <swathidj23@gmail.com>
2025-01-23 20:50:24 +0530

    Changing the conditions and commititng
    Changing the conditions and commititng

--- day2/IsPrime.java
+++ day2/IsPrime.java
@@ -1,18 +1,20 @@
 package week1.day2;
 
 public class IsPrime {
-	
+
 	public static void main(String[] args) {
-		int number = 15;
+		int number = 7;
+		int  conditionsatifies= 0; // to check if the number%i condition is pass or not
 		for (int i = 2; i < number; i++) {
-			if((i%number ==0)){
-				System.out.println(i+" : is a not Prime number");
-			}else
-			{
-				System.out.println(i+" : is a  Prime number");	
+			if ((number % i == 0)) {
+				conditionsatifies++;// if the condition is pass, then we need to increment the condition, so that this loop breaks and print the else defined at line16 
+				System.out.println(" : is a not Prime number");
+				break;
 			}
-			
+
+		}
+		if (conditionsatifies == 0) {
+			System.out.println(" : is a Prime number");
 		}
 	}
 }
-
